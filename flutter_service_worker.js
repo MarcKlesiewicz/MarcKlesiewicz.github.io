7e330a2b74849a1c0ae9ab28e0b5cd561e4df81d
Marc Klesiewicz <marcklesiewicz21@gmail.com>
2023-08-16 15:38:01 +0200

    Built web
    Branch: main
    Commit: ed7c7b9ba9c12b9838733a24c9deb26427f42586
    
    package:peanut 5.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -118,8 +118,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "af20fc4363da2e121a6ee2fd9f38fd3e
 "flutter.js": "6b515e434cea20006b3ef1726d2c8894",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "4f5004d893258428d580a2dede85c2cc",
-"/": "4f5004d893258428d580a2dede85c2cc",
+"index.html": "7791e954fa21f0f756c520ac2799e66e",
+"/": "7791e954fa21f0f756c520ac2799e66e",
 "main.dart.js": "5af28e6c9fec1b85c1e95e2d8aa53f24",
 "manifest.json": "246661e9355cf24b4f9cfc72cc000cc2",
 "version.json": "c4fa1175a6f24aea36519321aa977adf"};
